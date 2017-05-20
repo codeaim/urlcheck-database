442a7731703310e832f85bd36af4c90a3a9915bd
gdownes <gdownes@gmail.com>
2017-05-20 23:55:10 +0100

    Reduce responses kept to 1 day

--- src/createSchema.js
+++ src/createSchema.js
@@ -5,6 +5,59 @@ const pg = require('pg');
 module.exports.handler = (event, context, callback) => {
     const client = new pg.Client(process.env.DATABASE_URL);
     const sql = `
+        CREATE TABLE IF NOT EXISTS "user" (
+          username       VARCHAR(255) NOT NULL PRIMARY KEY,
+          email          VARCHAR(255) NOT NULL UNIQUE,
+          admin          BOOLEAN      NOT NULL,
+          created        TIMESTAMPZ   NOT NULL,
+          modified       TIMESTAMPZ   NOT NULL,
+          version        BIGINT       NOT NULL,
+          credit         BIGINT       NOT NULL
+        );
+    
+        CREATE TABLE IF NOT EXISTS "check" (
+          id         VARCHAR(8)    NOT NULL PRIMARY KEY,
+          username   VARCHAR(255)  NOT NULL REFERENCES "user" (username) ON DELETE CASCADE,
+          name       VARCHAR(255)  NOT NULL,
+          url        VARCHAR(2000) NOT NULL,
+          protocol   VARCHAR(5)    NOT NULL,
+          region     VARCHAR(255)  NULL,
+          status     VARCHAR(7)    NOT NULL CHECK (status IN ('UP', 'DOWN', 'UNKNOWN')) DEFAULT 'UNKNOWN',
+          state      VARCHAR(10)   NOT NULL CHECK (state IN ('WAITING', 'ELECTED'))     DEFAULT 'WAITING',
+          created    TIMESTAMPZ    NOT NULL,
+          modified   TIMESTAMPZ    NOT NULL,
+          refresh    TIMESTAMPZ    NOT NULL,
+          locked     TIMESTAMPZ    NULL,
+          disabled   TIMESTAMPZ    NULL,
+          interval   INT           NOT NULL,
+          confirming BOOLEAN       NOT NULL,
+          version    BIGINT        NOT NULL,
+          headers    JSON          NULL
+        );
+    
+        CREATE TABLE IF NOT EXISTS "change" (
+          id          BIGSERIAL  NOT NULL PRIMARY KEY,
+          check_id    BIGINT     NOT NULL REFERENCES "check" (id) ON DELETE CASCADE,
+          status      VARCHAR(4) NOT NULL CHECK (status IN ('UP', 'DOWN')),
+          status_code INT        NOT NULL,
+          created     TIMESTAMPZ NOT NULL
+        );
+    
+        CREATE TABLE IF NOT EXISTS "response" (
+          id       BIGSERIAL    NOT NULL PRIMARY KEY,
+          check_id BIGINT       NOT NULL REFERENCES "check" (id) ON DELETE CASCADE,
+          duration INT          NOT NULL,
+          region   VARCHAR(255) NULL,
+          created  TIMESTAMPZ   NOT NULL
+        );
+    
+        CREATE INDEX IF NOT EXISTS check_user_fkey
+          ON "check" (username);
+        CREATE INDEX IF NOT EXISTS change_check_fkey
+          ON "change" (check_id);
+        CREATE INDEX IF NOT EXISTS response_check_fkey
+          ON "response" (check_id);
+
         CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
         CREATE OR REPLACE FUNCTION unique_short_id()
@@ -40,7 +93,7 @@ module.exports.handler = (event, context, callback) => {
         RETURNS TRIGGER AS $$
 
         BEGIN
-          DELETE FROM response WHERE response.created < NOW() - INTERVAL '7 days';
+          DELETE FROM response WHERE response.created < NOW() - INTERVAL '1 day';
           RETURN NULL;
         END; $$ LANGUAGE plpgsql;
         
@@ -57,60 +110,7 @@ module.exports.handler = (event, context, callback) => {
           END IF;
           RETURN NULL;
         END; $$ LANGUAGE plpgsql;
-
-        CREATE TABLE IF NOT EXISTS "user" (
-          username       VARCHAR(255) NOT NULL PRIMARY KEY,
-          email          VARCHAR(255) NOT NULL UNIQUE,
-          admin          BOOLEAN      NOT NULL,
-          created        TIMESTAMP    NOT NULL,
-          modified       TIMESTAMP    NOT NULL,
-          version        BIGINT       NOT NULL,
-          credit         BIGINT       NOT NULL
-        );
-    
-        CREATE TABLE IF NOT EXISTS "check" (
-          id         VARCHAR(8)    NOT NULL PRIMARY KEY,
-          username   VARCHAR(255)  NOT NULL REFERENCES "user" (username) ON DELETE CASCADE,
-          name       VARCHAR(255)  NOT NULL,
-          url        VARCHAR(2000) NOT NULL,
-          protocol   VARCHAR(5)    NOT NULL,
-          region     VARCHAR(255)  NULL,
-          status     VARCHAR(7)    NOT NULL CHECK (status IN ('UP', 'DOWN', 'UNKNOWN')) DEFAULT 'UNKNOWN',
-          state      VARCHAR(10)   NOT NULL CHECK (state IN ('WAITING', 'ELECTED'))     DEFAULT 'WAITING',
-          created    TIMESTAMP     NOT NULL,
-          modified   TIMESTAMP     NOT NULL,
-          refresh    TIMESTAMP     NOT NULL,
-          locked     TIMESTAMP     NULL,
-          disabled   TIMESTAMP     NULL,
-          interval   INT           NOT NULL,
-          confirming BOOLEAN       NOT NULL,
-          version    BIGINT        NOT NULL,
-          headers    JSON          NULL
-        );
-    
-        CREATE TABLE IF NOT EXISTS "change" (
-          id          BIGSERIAL  NOT NULL PRIMARY KEY,
-          check_id    BIGINT     NOT NULL REFERENCES "check" (id) ON DELETE CASCADE,
-          status      VARCHAR(4) NOT NULL CHECK (status IN ('UP', 'DOWN')),
-          status_code INT        NOT NULL,
-          created     TIMESTAMP  NOT NULL
-        );
-    
-        CREATE TABLE IF NOT EXISTS "response" (
-          id       BIGSERIAL    NOT NULL PRIMARY KEY,
-          check_id BIGINT       NOT NULL REFERENCES "check" (id) ON DELETE CASCADE,
-          duration INT          NOT NULL,
-          region   VARCHAR(255) NULL,
-          created  TIMESTAMP    NOT NULL
-        );
-    
-        CREATE INDEX IF NOT EXISTS check_user_fkey
-          ON "check" (username);
-        CREATE INDEX IF NOT EXISTS change_check_fkey
-          ON "change" (check_id);
-        CREATE INDEX IF NOT EXISTS response_check_fkey
-          ON "response" (check_id);
-
+        
         DROP TRIGGER IF EXISTS check_generate_id
           ON "check";
 
